1a1674c419fd6c18686d9ec85e37f1f177036e5d
Sofie Sjöstedt Stålknapp <sofie.sjostedt.stalknapp@ica.se>
2018-10-18 15:33:39 +0200

    Setup xpress listViewer with vue

---
+++ Xpress.ListViewer/src/services/queque-service.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+let axiosInstance;
+
+const getApiClient = () => {
+  if (axiosInstance) return axiosInstance;
+
+  axiosInstance = axios.create({
+    baseURL: "http://sec31098.ica.ia-hc.net:8088/"
+  });
+
+  return axiosInstance;
+};
+
+function fetchTicket(id) {
+  return getApiClient()
+    .get(`api/queue/ticket/${id}`)
+    .then(response => response.data);
+}
+
+function fetchQueue(queueType) {
+  return getApiClient()
+    .get(`api/queue/${queueType}`)
+    .then(response => response.data);
+}
+
+function deleteTicket(id) {
+  return getApiClient()
+    .delete(`shoppinglists/${id}`);
+}
+
+function createTicket(ticket) {
+  return getApiClient()
+    .post(
+      '/api/queue/ticket',
+      { ticket },
+    );
+}
+
+export default {
+  fetchTicket,
+  fetchQueue,
+  deleteTicket,
+  createTicket,
+};
