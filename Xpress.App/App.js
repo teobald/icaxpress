58f4bf93c088235e700af82adfd3a35d5195c328
Magnus Härlin <magnus.harlin@ica.se>
2018-11-02 16:16:44 +0100

    gets data from backend and displays it in the app!

--- Xpress.App/App.js
+++ Xpress.App/App.js
@@ -23,23 +23,42 @@ export default class App extends Component<Props> {
   constructor (props) {
       super(props)
       this.state = {
-        queue: {}
+        queue: 0,
+        queueType: "",
+        nextNumber: undefined
       }
   }
 
   componentDidMount () {
+    this.setState((state) => {
+      return {queue: 35}
+    });
+    const baseurl = "http://sec31098.ica.ia-hc.net:8088/"
     return getQueue()
     .then((response) => {
-      this.queue = response;
+      // this.state.queue = response;
       console.log(response);
+      this.setState((state) => {
+        return {
+          queue: response.tickets[0].ticketNumber,
+          queueType: response.tickets[0].queueType,
+          nextNumber: response.nextTicketNumberToServe
+        }
+      });
     })
+    .catch(() => {
+      this.setState((state) => {
+        return {queue: "fail"}
+      });
+      })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Ditt nummer är: </Text>
-        <Text style={styles.instructions}>{this.queue}</Text>
+        <Text style={styles.welcome}>Ditt nummer för {this.state.queueType} är: </Text>
+        <Text style={styles.instructions}>{this.state.queue}</Text>
+        <Text style={styles.welcome}>Nästa nummer är: {this.state.nextNumber} </Text>
       </View>
     );
   }
--- Xpress.App/services.js
+++ Xpress.App/services.js
@@ -3,5 +3,6 @@ const baseurl = "http://sec31098.ica.ia-hc.net:8088/"
 export function getQueue() {
   return fetch(baseurl + 'api/queue/Deli', {
     method: 'GET'
-  });
+  })
+  .then(response => response.json());
 }
